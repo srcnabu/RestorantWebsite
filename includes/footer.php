e6a72a5b81e5ea0db3d6e05bc872e1df41227226
Steve Nebo <stv.nebo@gmail.com>
2018-03-06 22:51:59 +0200

    half way there

---
+++ includes/footer.php
@@ -0,0 +1,41 @@
+			<div id = "footer" class="cf">
+
+				<div class="column three">
+					<strong>Phone</strong>
+					29 884 4969
+				</div>
+
+				<div class="column three">
+					<strong>Location</strong>
+					Linnankatu 12, 20450<br>
+					Turku, FI
+				</div>
+
+				<div class="column three last">
+					
+					<strong>Hours</strong>
+					<em>Tuesday - Thursday</em><br>
+					13:00 - 22:00<br><br>
+
+					<em>Friday - Saturday</em><br>
+					16:00 - 24:00<br><br>
+
+					<em>Sunday - Monday</em><br>
+					closed<br><br>
+
+					<?php include 'includes/storeHours.php';  ?>
+
+				</div>
+				
+			</div>
+
+			<small>&copy; <?php echo date('Y'); ?> <?php echo $companyName; ?> </small>
+			
+		</div>
+		
+	</div>
+
+	<div class="copyright-info"></div>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<?php
+	$companyName = "Franklins dinner";
+	include 'includes/arrays.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo TITLE; ?></title>
+	<link rel="stylesheet" type="text/css" href="/assets/styles.css">
+</head>
+<body id="final-example">
+
+	<div class="wrapper">
+
+		<div id = "banner">
+
+			<a href="/" title="Return to Home">
+				<img src="img/banner.png" alt="Franklins dinner">
+			</a>
+			
+		</div>
+
+		<div id ="nav">
+				<?php include('includes/nav.php'); ?>
+		</div>
+
+		<div class="content">
